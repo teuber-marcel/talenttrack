68a52f172eb5b6a70fb001981312aa63f218743e
Felix-Mak <75482562+Felix-Mak@users.noreply.github.com>
2025-02-02 20:10:26 +0100

    Ajusted database model for vacancies and implemented the changes in the RadioButtonGroup and CheckboxGroup

--- client/src/components/CreateVacancy/CheckboxGroup.jsx
+++ client/src/components/CreateVacancy/CheckboxGroup.jsx
@@ -31,16 +31,16 @@ const CheckboxGroup = ({ onChange, disabled }) => {
           rowGap: "12px", 
           columnGap: "100px" 
         }}>
-          <Checkbox value="workingStudent" style={{ color: "white" }} disabled={disabled}>Working Student</Checkbox>
-          <Checkbox value="intern" style={{ color: "white" }} disabled={disabled}>Intern</Checkbox>
-          <Checkbox value="juniorProfessional" style={{ color: "white" }} disabled={disabled}>Junior Professional</Checkbox>
-          <Checkbox value="professional" style={{ color: "white" }} disabled={disabled}>Professional</Checkbox>
-          <Checkbox value="seniorProfessional" style={{ color: "white" }} disabled={disabled}>Senior Professional</Checkbox>
-          <Checkbox value="executiveProfessional" style={{ color: "white" }} disabled={disabled}>Executive Professional</Checkbox>
-          <Checkbox value="teamLead" style={{ color: "white" }} disabled={disabled}>Team Lead</Checkbox>
-          <Checkbox value="manager" style={{ color: "white" }} disabled={disabled}>Manager</Checkbox>
-          <Checkbox value="director" style={{ color: "white" }} disabled={disabled}>Director</Checkbox>
-          <Checkbox value="executiveDirector" style={{ color: "white" }} disabled={disabled}>Executive Director</Checkbox>
+          <Checkbox value="Working Student" style={{ color: "white" }} disabled={disabled}>Working Student</Checkbox>
+          <Checkbox value="Intern" style={{ color: "white" }} disabled={disabled}>Intern</Checkbox>
+          <Checkbox value="Junior Professional" style={{ color: "white" }} disabled={disabled}>Junior Professional</Checkbox>
+          <Checkbox value="Professional" style={{ color: "white" }} disabled={disabled}>Professional</Checkbox>
+          <Checkbox value="Senior Professional" style={{ color: "white" }} disabled={disabled}>Senior Professional</Checkbox>
+          <Checkbox value="Executive Professional" style={{ color: "white" }} disabled={disabled}>Executive Professional</Checkbox>
+          <Checkbox value="Team Lead" style={{ color: "white" }} disabled={disabled}>Team Lead</Checkbox>
+          <Checkbox value="Manager" style={{ color: "white" }} disabled={disabled}>Manager</Checkbox>
+          <Checkbox value="Director" style={{ color: "white" }} disabled={disabled}>Director</Checkbox>
+          <Checkbox value="Executive Director" style={{ color: "white" }} disabled={disabled}>Executive Director</Checkbox>
         </div>
       </Checkbox.Group>
     </div>
--- client/src/components/CreateVacancy/RadioButtonGroup.jsx
+++ client/src/components/CreateVacancy/RadioButtonGroup.jsx
@@ -31,16 +31,16 @@ const RadioButtonGroup = ({ onChange, disabled }) => {
           rowGap: "12px", 
           columnGap: "50px" 
         }}>
-          <Radio value="accounting" style={{ color: "white" }} disabled={disabled}>Accounting</Radio>
-          <Radio value="marketing" style={{ color: "white" }} disabled={disabled}>Marketing</Radio>
-          <Radio value="finance" style={{ color: "white" }} disabled={disabled}>Finance</Radio>
-          <Radio value="production" style={{ color: "white" }} disabled={disabled}>Production</Radio>
-          <Radio value="riskAndCompliance" style={{ color: "white" }} disabled={disabled}>Risk & Compliance</Radio>
-          <Radio value="projectManagement" style={{ color: "white" }} disabled={disabled}>Project Management</Radio>
-          <Radio value="humanResources" style={{ color: "white" }} disabled={disabled}>Human Resources</Radio>
-          <Radio value="researchAndDevelopment" style={{ color: "white" }} disabled={disabled}>Research & Development</Radio>
-          <Radio value="it" style={{ color: "white" }} disabled={disabled}>IT</Radio>
-          <Radio value="sales" style={{ color: "white" }} disabled={disabled}>Sales</Radio>
+          <Radio value="Accounting" style={{ color: "white" }} disabled={disabled}>Accounting</Radio>
+          <Radio value="Marketing" style={{ color: "white" }} disabled={disabled}>Marketing</Radio>
+          <Radio value="Finance" style={{ color: "white" }} disabled={disabled}>Finance</Radio>
+          <Radio value="Production" style={{ color: "white" }} disabled={disabled}>Production</Radio>
+          <Radio value="Risk & Compliance" style={{ color: "white" }} disabled={disabled}>Risk & Compliance</Radio>
+          <Radio value="Project Management" style={{ color: "white" }} disabled={disabled}>Project Management</Radio>
+          <Radio value="Human Resources" style={{ color: "white" }} disabled={disabled}>Human Resources</Radio>
+          <Radio value="Research & Development" style={{ color: "white" }} disabled={disabled}>Research & Development</Radio>
+          <Radio value="IT" style={{ color: "white" }} disabled={disabled}>IT</Radio>
+          <Radio value="Sales" style={{ color: "white" }} disabled={disabled}>Sales</Radio>
         </div>
       </Radio.Group>
     </div>
